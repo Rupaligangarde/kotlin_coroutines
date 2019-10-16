cc5bd0a8d37f18ffc1004ffebe61e81096e33eda
Rupali Gangarde <rupaligangarde11@gmail.com>
2019-10-16 13:18:18 +0530

    using dispatcher

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    exampleBlocking()
+    exampleBlockingDispatchers()
 }
 
 
@@ -13,8 +14,20 @@ suspend fun printlnDelay(message: String) {
 }
 
 
-fun exampleBlocking() = runBlocking {
+fun exampleBlocking() = runBlocking() {
     println("one")
     printlnDelay("two")
     println("three")
+}
+
+
+//runs on diff thread but still blocks main thread
+fun exampleBlockingDispatchers() {
+    runBlocking(Dispatchers.Default) {
+        println("one = from ${Thread.currentThread().name}")
+        printlnDelay("two = from ${Thread.currentThread().name}")
+    }
+
+    println("three = from ${Thread.currentThread().name}")
+    //this runs only after runBlocking is fully executed
 }
